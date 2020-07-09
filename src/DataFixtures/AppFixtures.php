af8d53c24cb364affe6d34e549a21c80149e5c04
Riot Nicolas <nico.riot@free.fr>
2020-07-09 21:19:35 +0200

    fixture img

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -30,12 +31,21 @@ class AppFixtures extends Fixture
                 ->setIntroduction($faker->paragraph(2))
                 ->setCoverImage($faker->imageUrl(1000,350));
     
+                for ($j = 1 ; $j <= mt_rand(2,5) ; $j ++){
+
+                    $image = new Image();
+                    $image->setUrl($faker->imageUrl())
+                    ->setCaption($faker->sentence())
+                    ->setAd($ad);
+
+                    $manager->persist($image);
+
+
+                }
+
                 $manager->persist($ad);
 
-
         }
-
-
         $manager->flush();
 
 
