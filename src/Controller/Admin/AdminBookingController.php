b2eeb312379027aaa0c3324fd7c6a9aee5553534
nicoWeb31 <nico.riot@free.fr>
2020-08-04 14:41:08 +0200

    booking edit

--- src/Controller/Admin/AdminBookingController.php
+++ src/Controller/Admin/AdminBookingController.php
@@ -2,19 +2,55 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Booking;
+use App\Form\AdminBookingType;
 use App\Repository\BookingRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminBookingController extends AbstractController
 {
     /**
      * @Route("/admin/booking", name="admin_booking_index")
+     * @return response
      */
     public function index(BookingRepository $repo)
     {
         return $this->render('admin/admin_booking/index.html.twig', [
             'bookings'=>$repo->findAll(),
         ]);
+    }
+
+
+    /**
+     * for editing a booking
+     * @Route("/admin/booking/{id}/edit", name="admin_booking_edit")
+     * @return response
+     */
+    public function edit(Booking $booking,Request $req, EntityManagerInterface $man)
+    {
+
+        $form = $this->createForm(AdminBookingType::class,$booking);
+
+        $form->handleRequest($req);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $man->persist($booking);
+            $man->flush();
+
+            $this->addFlash('success',"la réservation n° {$booking->getId()} a bien été modifier !");
+
+            return $this->redirectToRoute('admin_booking_index');
+        }
+
+
+        return $this->render('admin/admin_booking/edit.html.twig', [
+            'form'=>$form->createView(),
+            'booking'=>$booking
+        ]);
+
     }
 }
--- src/Form/AdminBookingType.php
+++ src/Form/AdminBookingType.php
@@ -2,8 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Ad;
 use App\Entity\Booking;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +16,25 @@ class AdminBookingType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('startDate')
-            ->add('endDate')
+            ->add('startDate',DateType::class,[
+                'widget' => 'single_text'
+            ])
+            ->add('endDate',DateType::class,[
+                'widget' => 'single_text'
+            ])
             ->add('comment')
-            ->add('booker')
-            ->add('ad')
+            // relier un entité 
+            ->add('booker',EntityType::class,[
+                
+                'class'=>User::class,                        //quel class
+                'choice_label'=> function($user){            //label
+                    return $user->getFirstName(). " " .strtoupper($user->getLastName());
+                }                             
+            ])
+            ->add('ad',EntityType::class,[
+                'class' => Ad::class,
+                'choice_label' => 'title'
+            ])
         ;
     }
 
